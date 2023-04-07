961bbd33591c0b2939f737484ff5148873735038
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-07 23:24:31 +0100

    more connect

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
   try {
-    
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING)
   } catch (err) {
     console.log(err);
     process.exit(1)
     
   }
-}
+};
+
+module.exports = connectDB;
