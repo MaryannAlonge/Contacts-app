d08437086fc112effd4b825d6a27f4a7890ba3d6
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-04-08 22:30:47 +0100

    more user schema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,17 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel")
 
 //@desc Register a User
 //@route POST /user/register
 //@access public
 
 const registerUser = asyncHandler (async (req, res) => {
+  const {username, email, password} = req.body;
+  if (!username || !email || !password){
+    res.status(400);
+    throw new Error("All fields are mandatory")
+  }
+  const userAvailable = await 
   res.json({message: "Register the user"});
 });
 
